c87e0eb6d08d47de523d548e620cae7d6fc19b3a
Samy Labassi <samy.labassi@gmail.com>
2020-09-21 15:45:12 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -15,25 +15,42 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
-
+    counter = 0
+    for i in string:
+        if i == char:
+            counter += 1
+    return counter
+#count = 0
+#    if(sentence) : count = 1
+#    for char in sentence :
+#        if(char == ' ') : count += 1
+#    return count
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    #len(sentence.split())
+    counter = 0
+    if(sentence) :
+        counter = 1
+    for char in sentence:
+        if char == " " :
+            counter += 1
+
+    return counter
 
 
 def main() -> None:
-    string = "Bonjour!"
-    parity = 'pair' if is_even_len(string) else 'impair'
-    print(f"Le nombre de caractère dans la chaine '{string}' est {parity}")
+    #string = "Bonjour!"
+    #parity = 'pair' if is_even_len(string) else 'impair'
+    #print(f"Le nombre de caractère dans la chaine '{string}' est {parity}")
 
-    string = "Sam est cool!"
-    print(f"On supprime le 3e caratère dans la chaine '{string}'. Résultat: {remove_third_char(string)}")
+    #string = "Sam est cool!"
+    #print(f"On supprime le 3e caratère dans la chaine '{string}'. Résultat: {remove_third_char(string)}")
 
-    string = "hello world!"
-    print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
+    #string = "hello world!"
+    #print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
+    #string = "Hello"
+    #print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
     
     string = "Baby shark doo doo doo doo doo doo"
     print(f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
